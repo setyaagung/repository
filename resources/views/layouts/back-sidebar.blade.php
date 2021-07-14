cd47d0b66bd26d63dabc67f21db13615172f36a9
Muhammad Agung Setya Budi <setyaagung74@gmail.com>
2021-07-14 22:18:26 +0700

    update logo

--- resources/views/layouts/back-sidebar.blade.php
+++ resources/views/layouts/back-sidebar.blade.php
@@ -2,7 +2,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="#" class="brand-link">
-        <img src="{{ asset('assets/dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <img src="{{ asset('img/logo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light"><b>STIE CENDEKIAKU</b></span>
     </a>
 
