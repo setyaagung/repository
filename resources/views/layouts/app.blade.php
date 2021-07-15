8ea725d2cf473ab258d31c02a6a3699432fe6f97
Muhammad Agung Setya Budi <setyaagung74@gmail.com>
2021-07-15 14:15:40 +0700

    repo simple done

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
 </head>
 <body>
     <header>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <nav class="navbar navbar-expand-lg navbar-light" style="background: #eaeaea">
             <div class="container">
                 <a class="navbar-brand" href="/">
                     <img src="{{ asset('img/logo-stie-cku.png')}}" class="img-fluid" alt="">
